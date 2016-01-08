ae34cb6b6d1be8038464b476f1c41df7fcd911f4
ayraei <ayraei@users.noreply.github.com>
2016-01-08 00:31:51 -0800

    Moved HW0 answers from Dropbox

---
+++ HW0/lab0.ml
@@ -0,0 +1,27 @@
+(* QUESTION 1 *)
+(* (1) 4.2.0-16-generic *)
+(* (2) No response. *)
+(* (3) No response. *)
+
+(* QUESTION 2 *)
+(* (1) 4.2.0-23-generic *)
+(* (2) No response. *)
+(* (3) No response. *)
+(* The response for 'uname -r' has changed since updating. *)
+
+(* QUESTION 3 *)
+(* (1) 4.2.0-23-generic *)
+(* (2) /usr/bin/gcc *)
+(* (3) No response. *)
+(* It appears that gcc has been installed via Guest Utilities. *)
+
+(* QUESTION 4 *)
+(* (1) /usr/bin/ocaml *)
+(* (2) /usr/bin/opam *)
+(* Since ocaml and opam installed, there are real responses. *)
+
+(* QUESTION 5 *)
+(* Ocaml prints
+ * val f : int -> int = <fun>
+ * val g : float -> float -> float = <fun>
+ *)
