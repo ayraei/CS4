f27d019c874e0249d3e33b2fe0ba000e76dd4e4a
Joanne <joanne@caltech.edu>
2016-01-29 00:13:18 -0800

    HW2 first submission.

---
+++ HW2/lab2.ml
@@ -0,0 +1,24 @@
+(* Sorry Nick! *)
+(* PART A *)
+
+(* Question 1 *)
+(* O(n) because the applicative-order evaluation will evaluate one operand
+ * fully (and one operand of the statement substituted in, etc) before
+ * continuing to the next operand.
+ *)
+
+(* Question 2 *)
+(* part a *)
+
+
+(* Question 3 *)
+
+(* Question 4 *)
+
+(* Question 5 *)
+
+(* Question 6 *)
+
+(* Question 7 *)
+
+(* PART B *)
